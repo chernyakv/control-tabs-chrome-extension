57f7cecd4718262fa5621fb6160d91c656a878dd
chernyakVladislav <chernyak.chery@gmail.com>
2019-12-19 17:24:15 +0300

    fix regexp

--- js/background.js
+++ js/background.js
@@ -3,20 +3,41 @@ var maxNumberOfOpenTabs = 6;
 var numberOfOpenTabs = 1;
 var parentTabsMap = {};
 
+/*
+ * Удаляет вкладку, если лимит открытых вкладок превышен.
+ */
 function deleteIfLimitIsExceeded(newTabId) {
   if (numberOfOpenTabs >= maxNumberOfOpenTabs) {
     chrome.tabs.remove(newTabId);
   }
   updateBadgeText();
 }
-
+/*
+ *  Сверяет URL для перехода со списоком проеверяемых URL.
+ */
+function checkWithCkechedList(navigationUrl) {
+  var result = false;
+  for (let item of checkedUrlList) {
+    console.log(item);
+    if (new RegExp(item, "i").test(navigationUrl)) {
+      result = true;
+      break;
+    }
+  }
+  return result;
+}
+/*
+ * Обновляет количество открытых вкладок.
+ */
 function updateNumberOfOpenTabs() {
-  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+    chrome.tabs.query({ currentWindow: true }, function (tabs) {
     numberOfOpenTabs = tabs.length;
     updateBadgeText();
   });
 }
-
+/*
+ * Обновляет текст на значке расширения.
+ */
 function updateBadgeText() {
   chrome.browserAction.setBadgeText({
     text: "" + numberOfOpenTabs
@@ -24,15 +45,19 @@ function updateBadgeText() {
   const color = numberOfOpenTabs == maxNumberOfOpenTabs ? "red" : "green";
   chrome.browserAction.setBadgeBackgroundColor({ color });
 }
-
-function removeFromMapIfItDependentTab(tabId) {
+/*
+ * Удаляет зависимость, если удаляемая вкладка является зависимой.
+ */
+function removeFromMapIfItDependentTab(removedTabId) {
   for (key in parentTabsMap) {
-    if (parentTabsMap[key].childTabId == tabId) {
+    if (parentTabsMap[key].childTabId == removedTabId) {
       delete parentTabsMap[key];
     }
   }
 }
-
+/*
+ *  Обнавляет значения из chrome.storage.
+ */
 function onStorageChangedHandler(changes, namespace) {
   for (key in changes) {
     newValue = changes[key].newValue;
@@ -44,15 +69,20 @@ function onStorageChangedHandler(changes, namespace) {
     }
   }
 }
-
+/*
+ *  Вызывается после создания новой вкладки.
+ */
 function onTabCreatedHandler(tab) {
   updateNumberOfOpenTabs();
   deleteIfLimitIsExceeded(tab.id);
+  //Позиционирует вкладку слева, если нет родительской вкладки.
   if (!tab.openerTabId) {
     chrome.tabs.move(tab.id, { windowId: null, index: 0 });
   }
 }
-
+/*
+ *  Вызывается после удаления вкладки.
+ */
 function onTabRemovedHandler(removedTabId) {
   updateNumberOfOpenTabs();
   removeFromMapIfItDependentTab(removedTabId);
@@ -61,23 +91,12 @@ function onTabRemovedHandler(removedTabId) {
     delete parentTabsMap[removedTabId];
   }
 }
-
-function checkWithCkechedList(navigationUrl) {
-  var isMathc = false;
-  var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi; 
-  var regex = new RegExp(expression);
-  checkedUrlList.forEach(item => {      
-    if(item.match(regex)[0] === navigationUrl.match(regex)[0]) {     
-      isMathc = true;
-    }
-  })
-  return isMathc;
-}
-
+/*
+ *  Вызывается, когда создается новая вкладка для навигации.
+ */
 function onCreatedNavigationTargetHandler(details) {
   const isMatch = checkWithCkechedList(details.url);
-  console.log(isMatch);
-  if (checkedUrlList.indexOf(details.url) !== -1) {
+  if (isMatch) {
     const deps = parentTabsMap[details.sourceTabId]
     if (deps) {
       chrome.tabs.update(deps.childTabId, { url: details.url }, function (tab) {
@@ -91,7 +110,9 @@ function onCreatedNavigationTargetHandler(details) {
     }
   }
 }
-
+/*
+ *  Устанавливает начальные значения.
+ */
 function setDefaults() {
   chrome.storage.sync.get(null, function (items) {
     if (items.checkedUrlList) {
--- js/popup.js
+++ js/popup.js
@@ -1,75 +1,82 @@
 function Rule(data) {
-    var rules = document.getElementById('rules');
-    this.node = document.getElementById('rule-template').cloneNode(true);
-    this.node.id = 'rule' + (Rule.next_id++);
-    this.node.rule = this;
-    rules.appendChild(this.node);
-    this.node.hidden = false;
-  
-    if (data) {    
-      this.getElement('url-param').value = data.url;
-      this.getElement('enabled').checked = data.enabled;
+  var rules = document.getElementById('rules');
+  this.node = document.getElementById('rule-template').cloneNode(true);
+  this.node.id = 'rule' + (Rule.next_id++);
+  this.node.rule = this;
+  rules.appendChild(this.node);
+  this.node.hidden = false;
+
+  if (data) {
+    this.getElement('url-param').value = data.url;
+    this.getElement('enabled').checked = data.enabled;
+  }
+
+  this.getElement('enabled-label').htmlFor = this.getElement('enabled').id =
+    this.node.id + '-enabled';
+
+  this.getElement('url-param').onkeyup = storeRules;
+  this.getElement('enabled').onchange = storeRules;
+
+  var rule = this;
+
+  this.getElement('remove').onclick = function () {
+    rule.node.parentNode.removeChild(rule.node);
+    storeRules();
+  };
+  storeRules();
+}
+
+Rule.prototype.getElement = function (name) {
+  return document.querySelector('#' + this.node.id + ' .' + name);
+}
+
+Rule.next_id = 0;
+/*
+ *  Выгружает список проверяемых URL из chrome.storage.
+ */
+function loadRules() {
+  chrome.storage.sync.get('checkedUrlList', function (items) {
+    if (items.checkedUrlList) {
+      items.checkedUrlList.forEach(item => {
+        new Rule(item);
+      });
     }
-  
-    this.getElement('enabled-label').htmlFor = this.getElement('enabled').id =
-      this.node.id + '-enabled';
-
-    this.getElement('url-param').onkeyup = storeURLs;  
-    this.getElement('enabled').onchange = storeURLs;
-  
-    var rule = this;  
-  
-    this.getElement('remove').onclick = function() {
-      rule.node.parentNode.removeChild(rule.node);
-      storeURLs();
-    };
-    storeURLs();
-  }
-  
-  Rule.prototype.getElement = function(name) {
-    return document.querySelector('#' + this.node.id + ' .' + name);
-  }
-  
-  Rule.next_id = 0;
-  
-  function loadURLs() {
-    chrome.storage.sync.get('checkedUrlList', function (items) {
-        if (items.checkedUrlList) {
-            items.checkedUrlList.forEach(item => {
-                new Rule(item);               
-            });
-        }
-    }) 
-  }
-  
-  function storeURLs() {
-    chrome.storage.sync.set({ 'checkedUrlList': Array.prototype.slice.apply(
-        document.getElementById('rules').childNodes).map(function(node) {
+  })
+}
+/*
+ *  Записывает список проверяемых URL в chrome.storage.
+ */
+function storeRules() {
+  chrome.storage.sync.set({
+    'checkedUrlList': Array.prototype.slice.apply(
+      document.getElementById('rules').childNodes).map(function (node) {
         return {
-            url: node.rule.getElement('url-param').value,
-            enabled: node.rule.getElement('enabled').checked
+          url: node.rule.getElement('url-param').value,
+          enabled: node.rule.getElement('enabled').checked
         };
-    })});   
-  }
+      })
+  });
+}
+/*
+ *  Записывает максимальное количество открытых вкладок в chrome.storage.
+ */
+function storeMaxNumberOfOpenTabs() {
+  const maxNumberOfOpenTabs = document.getElementById('max-number-input').value;
+  chrome.storage.sync.set({ maxNumberOfOpenTabs });
+}
 
-  function storeMaxNumberOfOpenTabs() { 
-    const maxNumberOfOpenTabs = document.getElementById('max-number-input').value;
-    chrome.storage.sync.set({ maxNumberOfOpenTabs });     
-  }
+window.onload = function () {
+  loadRules();
+  chrome.storage.sync.get('maxNumberOfOpenTabs', function (items) {
+    if (items.maxNumberOfOpenTabs) {
+      document.getElementById('max-number-input').value = items.maxNumberOfOpenTabs;
+    }
+  })
 
-  window.onload = function() {
-    loadURLs();
-    chrome.storage.sync.get('maxNumberOfOpenTabs', function (items) {
-        if (items.maxNumberOfOpenTabs) {
-            document.getElementById('max-number-input').value = items.maxNumberOfOpenTabs;
-        }
-    }) 
-   
-    document.getElementById('new').onclick = function() {
-      new Rule();
-    };
-    document.getElementById('save').onclick = function() {
-        storeMaxNumberOfOpenTabs();
-    };
-  }
-  
+  document.getElementById('new').onclick = function () {
+    new Rule();
+  };
+  document.getElementById('save').onclick = function () {
+    storeMaxNumberOfOpenTabs();
+  };
+}
